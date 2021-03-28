2f8410780a5a33a21c3478615e2719b946ba0efd
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2021-03-27 23:55:46 -0300

    grad view

---
+++ lib/home/sections/CoursesSection.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CoursesSection extends StatelessWidget {
+  const CoursesSection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 300,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+          ),
+          padding: EdgeInsets.symmetric(
+              vertical: 16, horizontal: constraints.maxWidth >= 1200 ? 0 : 16),
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return Container(color: Colors.red);
+          },
+        );
+      },
+    );
+  }
+}
