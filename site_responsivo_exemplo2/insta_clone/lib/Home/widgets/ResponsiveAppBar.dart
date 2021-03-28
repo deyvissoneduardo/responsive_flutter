9debd265e702726204db3a293931df88ab83034b
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2021-03-28 19:23:35 -0300

    field search'

---
+++ site_responsivo_exemplo2/insta_clone/lib/Home/widgets/ResponsiveAppBar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ResponsiveAppBar extends StatelessWidget {
+  const ResponsiveAppBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      title: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 1200),
+        child: Row(
+          children: [
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Text(
+                'Flutter',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontFamily: 'Billabong',
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 30,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white)),
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 14,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Expanded(
+                      child: TextField(
+                          style: TextStyle(fontSize: 14, color: Colors.white),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            isCollapsed: true,
+                          )))
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
