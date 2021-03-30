6a5ad5096d346922c12e304bd31bb106c52ce7fb
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2021-03-29 22:11:15 -0300

    row storys

--- site_responsivo_exemplo2/insta_clone/lib/Home/HomePage.dart
+++ site_responsivo_exemplo2/insta_clone/lib/Home/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/Home/widgets/ResponsiveAppBar.dart';
+import 'package:insta_clone/Home/widgets/StoryList.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -7,9 +8,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-      preferredSize: Size(double.infinity, 56),
-      child: ResponsiveAppBar(),
-    ));
+      backgroundColor: Colors.black87,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 56),
+        child: ResponsiveAppBar(),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 1000),
+          child: ListView(
+            children: [StoryList()],
+          ),
+        ),
+      ),
+    );
   }
 }
--- site_responsivo_exemplo2/insta_clone/lib/Home/widgets/ResponsiveAppBar.dart
+++ site_responsivo_exemplo2/insta_clone/lib/Home/widgets/ResponsiveAppBar.dart
@@ -11,67 +11,70 @@ class ResponsiveAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.black,
-      title: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 1200),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: Text(
-                  'Flutter',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontFamily: 'Billabong',
-                      fontWeight: FontWeight.w500),
+      title: Align(
+        alignment: Alignment.center,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 1200),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: Text(
+                    'Flutter',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontFamily: 'Billabong',
+                        fontWeight: FontWeight.w500),
+                  ),
                 ),
               ),
-            ),
-            ResponsiveVisibility(
-              visible: false,
-              visibleWhen: [Condition.largerThan(name: MOBILE)],
-              child: Expanded(
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    width: 200,
-                    height: 30,
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.white)),
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.search,
-                          color: Colors.white,
-                          size: 14,
-                        ),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        Expanded(
-                            child: TextField(
-                                style: TextStyle(
-                                    fontSize: 14, color: Colors.white),
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  isCollapsed: true,
-                                )))
-                      ],
+              ResponsiveVisibility(
+                visible: false,
+                visibleWhen: [Condition.largerThan(name: MOBILE)],
+                child: Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 200,
+                      height: 30,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white)),
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.white,
+                            size: 14,
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Expanded(
+                              child: TextField(
+                                  style: TextStyle(
+                                      fontSize: 14, color: Colors.white),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    isCollapsed: true,
+                                  )))
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            ResponsiveVisibility(
-              visible: false,
-              visibleWhen: [Condition.largerThan(name: MOBILE)],
-              child: Expanded(child: ExpendedResponsive()),
-              replacement: ExpendedResponsive(),
-            ),
-          ],
+              ResponsiveVisibility(
+                visible: false,
+                visibleWhen: [Condition.largerThan(name: MOBILE)],
+                child: Expanded(child: ExpendedResponsive()),
+                replacement: ExpendedResponsive(),
+              ),
+            ],
+          ),
         ),
       ),
     );
